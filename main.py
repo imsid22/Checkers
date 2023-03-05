ed8e6ce03cb7c3af8ff38f1c4b10a9985e5f6ab7
imsid22 <siddhantd94@gmail.com>
2023-03-05 00:02:37 -0700

    Fix: THe dimensions of the board game

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pygame
 pygame.init()
 
 # Set up the game window
-WINDOW_SIZE = (640, 480)
+WINDOW_SIZE = (500, 500)
 screen = pygame.display.set_mode(WINDOW_SIZE)
 
 # Set up the game loop
@@ -18,6 +18,8 @@ LIGHT_BROWN = (204, 102, 0)
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 
+
+
 while game_running:
     # Handle user input
     for event in pygame.event.get():
@@ -27,8 +29,9 @@ while game_running:
     # Update the game state
 
     # Draw the game graphics
-    CELL_SIZE = WINDOW_SIZE[1] // checker_board.n # set the cell size to fit 8 rows
+    CELL_SIZE = WINDOW_SIZE[0] // checker_board.n # set the cell size to fit 8 rows
     RADIUS = (CELL_SIZE // 2) - 10  # set the radius of the checkers
+
 
     for row in range(checker_board.n):
         for col in range(checker_board.n):
@@ -46,6 +49,7 @@ while game_running:
                 pygame.draw.circle(screen, color, (col * CELL_SIZE + CELL_SIZE // 2, row * CELL_SIZE + CELL_SIZE // 2), RADIUS)
 
     pygame.display.flip()  # update the screen
+
 
 # Clean up Pygame
 pygame.quit()
